5eba871f26458766fd2fe6aa0914aa2a1f3c525b
Abed <abedhamami42@gmail.com>
2023-01-12 21:51:49 +0200

    fixed checkbox bug and fetching data from db

--- emoji-to-do/script.js
+++ emoji-to-do/script.js
@@ -1,47 +1,61 @@
-const todos = [
-    {
-        userId: 1,
-        id: 0,
-        emoji: '🏃',
-        title: "go running ",
-        completed : true
-    },
-    {
-        userId: 1,
-        id: 1,
-        emoji: '🏃',
-        title: "take cold shower",
-        completed : false
-    },
-    {
-        userId: 1,
-        id: 2,
-        emoji: '🏃',
-        title: "cook dinner",
-        completed : true
-    },
-    {
-        userId: 1,
-        id: 3,
-        emoji: '🏃',
-        title: "wash the dishes",
-        completed : false
-    },
-    {
-        userId: 1,
-        id: 4,
-        emoji: '🏃',
-        title: "do the homework",
-        completed : true
-    },
+// const todos = [
+//     {
+//         userId: 1,
+//         id: 0,
+//         emoji: '🏃',
+//         title: "go running ",
+//         completed : true
+//     },
+//     {
+//         userId: 1,
+//         id: 1,
+//         emoji: '🏃',
+//         title: "take cold shower",
+//         completed : false
+//     },
+//     {
+//         userId: 1,
+//         id: 2,
+//         emoji: '🏃',
+//         title: "cook dinner",
+//         completed : true
+//     },
+//     {
+//         userId: 1,
+//         id: 3,
+//         emoji: '🏃',
+//         title: "wash the dishes",
+//         completed : false
+//     },
+//     {
+//         userId: 1,
+//         id: 4,
+//         emoji: '🏃',
+//         title: "do the homework",
+//         completed : true
+//     },
 
-];
+// ];
+
+let todos;
+
+fetch('https://jsonplaceholder.typicode.com/todos/')
+.then(response =>  response.json())
+.then(data => todos = data)
+.then(()=>{
+    for(let i = 0 ;i < todos.length; i++) {
+        renderTask(todos[i].emoji,todos[i].title,todos[i].completed)
+    }
+    triggerLineThrough();
+})
+
+
 
 const taskContainer = document.querySelector('#the-one');
 const taskInput = document.querySelector('.task-input');
 const emojiInput = document.querySelector('.emoji-input');
 
-function renderTask(emoji, title, completed  ) {
+function renderTask(emoji, title, completed) {
     const taskCard = document.createElement('li'); 
     taskCard.className = 'card'
     taskCard.innerHTML =`
@@ -52,31 +66,37 @@ function renderTask(emoji, title, completed  ) {
     taskContainer.appendChild(taskCard) 
 }
 
-for(let i = 0 ;i < todos.length; i++) {
-    renderTask(todos[i].emoji,todos[i].title,todos[i].completed)
+
+
+
+function triggerLineThrough() {
+    const checkboxInputs = document.querySelectorAll('.checkbox');
+
+    checkboxInputs.forEach(function(checkboxInput ){
+
+        checkboxInput.addEventListener('change', () => {
+            if (checkboxInput.checked) {
+                checkboxInput.previousElementSibling.style.textDecoration ='line-through'
+            } else {
+                checkboxInput.previousElementSibling.style.textDecoration ='none' 
+            }
+            // checkboxInput.previousElementSibling.style.textDecoration ='line-through'
+        })
+        if (checkboxInput.checked) {
+            checkboxInput.previousElementSibling.style.textDecoration ='line-through'
+        }
+    })
+    
 }
 
+
 function addTask() {
+
     if(taskInput.value !== '') {
         renderTask(emojiInput.value,taskInput.value, false )
         taskInput.value = '';
+        taskInput.value = '';
+        triggerLineThrough();
+
     }
 }
-
-const checkboxInputs = document.querySelectorAll('.checkbox');
-
-checkboxInputs.forEach(function(checkboxInput ){
-
-    checkboxInput.addEventListener('change', () => {
-        if (checkboxInput.checked) {
-            checkboxInput.previousElementSibling.style.textDecoration ='line-through'
-        } else {
-            checkboxInput.previousElementSibling.style.textDecoration ='none'
- 
-        }
-        // checkboxInput.previousElementSibling.style.textDecoration ='line-through'
-    })
-    if (checkboxInput.checked) {
-        checkboxInput.previousElementSibling.style.textDecoration ='line-through'
-    }
-})
